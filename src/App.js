cde9acdf3dded1358caa19b66097256a364ac797
Harshil Patel <harshilcpatel@gmail.com>
2024-08-09 13:14:20 +0530

    comit it

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./About";
 import Arts from "./Art";
 import Signup from "./Components/Signup"
 import Contact from "./Contact";
-import Dragonball from "./Components/Dragonball";
+
 
 
 const App = () => {
@@ -18,7 +18,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Arts" element={<Arts />} />
-          <Route path="/Arts/Dragonball" element={<Dragonball />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
